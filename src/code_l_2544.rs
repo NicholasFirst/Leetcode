14678ddb44d11b459baa1b62c2973bcceefd1b24
Nicholas <17010289393@163.com>
2023-10-08 22:57:03 +0800

    2544 insert

--- src/code_l_2544.rs
+++ src/code_l_2544.rs
@@ -1,8 +1,15 @@
 struct Solution;
 
+#[allow(dead_code)]
 impl Solution {
-    pub fn alternate_digit_sum(n: i32) -> i32 {
-
+    pub fn alternate_digit_sum(mut n: i32) -> i32 {
+        let (mut ret, mut k) = (0, 1);
+        while n != 0 {
+            ret += k * (n % 10);
+            k = -k;
+            n /= 10;
+        }
+        -k * ret
     }
 }
 

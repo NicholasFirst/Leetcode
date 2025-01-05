e42201f43b0d67cf0bbb6b303ffb4f59d65f8fcd
Nicholas <17010289393@163.com>
2025-01-05 17:48:05 +0800

    2241 update

--- src/code_m_2241.rs
+++ src/code_m_2241.rs
@@ -1,24 +1,23 @@
-#[derive(Default, Debug)]
 struct ATM {
     cnt: Vec<i64>,   // 每张钞票剩余数量
-    value: Vec<i64>,   // 每张钞票面额
+    value: Vec<i64>, // 每张钞票面额
 }
 
+#[allow(dead_code)]
 impl ATM {
-
     fn new() -> Self {
         ATM {
             cnt: vec![0, 0, 0, 0, 0],
             value: vec![20, 50, 100, 200, 500],
         }
     }
-    
+
     fn deposit(&mut self, banknotes_count: Vec<i32>) {
         for i in 0..5 {
             self.cnt[i] += banknotes_count[i] as i64;
         }
     }
-    
+
     fn withdraw(&mut self, mut amount: i32) -> Vec<i32> {
         let mut res = vec![0; 5];
         // 模拟尝试取出钞票的过程
@@ -46,8 +45,10 @@ mod tests {
     #[test]
     fn test_1() {
         let mut atm = ATM::new();
-        atm.deposit(vec![0,0,1,2,1]);
-        dbg!(atm);
-        // assert_eq!(atm.withdraw(200), vec![0,1,1,0,0]);
+        atm.deposit(vec![0, 0, 1, 2, 1]);
+        assert_eq!(atm.withdraw(600), vec![0, 0, 1, 0, 1]);
+        atm.deposit(vec![0, 1, 0, 1, 1]);
+        assert_eq!(atm.withdraw(600), vec![-1]);
+        assert_eq!(atm.withdraw(550), vec![0, 1, 0, 0, 1])
     }
 }

41109fadb59b2822c8c1f8109fcc783501628a87
Nicholas <17010289393@163.com>
2025-05-06 18:48:03 +0800

    2099 - update

--- src/code_l_2099.rs
+++ src/code_l_2099.rs
@@ -2,9 +2,6 @@ struct Solution;
 
 impl Solution {
     // https://leetcode.cn/problems/find-subsequence-of-length-k-with-the-largest-sum/?envType=problem-list-v2&envId=sorting
-    // 给你一个整数数组 nums 和一个整数 k 。你需要找到 nums 中长度为 k 的 子序列 ，且这个子序列的 和最大 。
-    // 请你返回 任意 一个长度为 k 的整数子序列。
-    // 子序列 定义为从一个数组里删除一些元素后，不改变剩下元素的顺序得到的数组。
     #[allow(dead_code)]
     pub fn max_subsequence(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let mut a = Vec::with_capacity(nums.len());

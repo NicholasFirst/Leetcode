9ed6df8e875ac37aa7cce6c9e31ae714879de50c
Nicholas <17010289393@163.com>
2025-05-24 13:31:15 +0800

    2942优化

--- src/code_l_2942.rs
+++ src/code_l_2942.rs
@@ -4,9 +4,16 @@ impl Solution {
     // https://leetcode.cn/problems/find-words-containing-character/?envType=daily-question&envId=2025-05-24
     #[allow(dead_code)]
     pub fn find_words_containing(words: Vec<String>, x: char) -> Vec<i32> {
-        words.iter().enumerate().filter(
-            |(_, s)| s.contains(x)
-        ).map(|(i, _)| i as i32).collect::<Vec<i32>>()
+        // words.iter().enumerate().filter(
+        //     |(_, s)| s.contains(x)
+        // ).map(|(i, _)| i as i32).collect::<Vec<i32>>()
+        words.iter().enumerate().filter_map(|(i, s)| {
+            if s.contains(x) {
+                Some(i as i32)
+            } else {
+                None
+            }
+        }).collect::<Vec<i32>>()
     }
 }
 
